3ed223f38428f2a4cd16b0134a6930828a448290
EdwardTuring <mxturing@yeah.net>
2012-02-08 23:56:17 +0800

    added quazip lib files;thanks the quazip project's contributors

--- tool/pakger/main.cpp
+++ tool/pakger/main.cpp
@@ -1,9 +1,9 @@
 #include <QtCore>
 #include "quazip/quazipfile.h"
+#include "quazip/JlCompress.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QuaZipFile zip();
-
+    qDebug()<<JlCompress::compressDir("aa","ss");
     return a.exec();
 }
